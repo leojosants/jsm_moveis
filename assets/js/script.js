b70519cc4008811a97ab643db31de389624976fb
Leonardo Santos <leonardojosantos@gmail.com>
2024-05-08 01:20:15 -0300

    tempo de video 1:46:36

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@ let DOM_closer = document.querySelector('#id-closer');
 let DOM_navbar = document.querySelector('.c-navbar');
 let DOM_cart = document.querySelector('.c-shopping-cart');
 let DOM_loginForm = document.querySelector('.c-login-form');
+let DOM_searchForm = document.querySelector('.c-header .c-search-form');
+let DOM_slides = document.querySelectorAll('.c-home .c-slides-container .c-slide');
+let index = 0;
 
 DOM_closer.onclick = () => {
     DOM_closer.style.display = 'none';
@@ -24,3 +27,25 @@ document.querySelector('#id-login-btn').onclick = () => {
     DOM_closer.style.display = 'block';
     DOM_loginForm.classList.toggle('c-active');
 };
+
+// analisar
+document.querySelector('#id-search-btn').onclick = () => {
+    alert()
+    DOM_searchForm.classList.toggle('c-active');
+};
+
+window.onscroll = () => {
+    DOM_searchForm.classList.remove('c-active');
+};
+
+function next() {
+    DOM_slides[index].classList.remove('c-active');
+    index = (index + 1) % DOM_slides.length;
+    DOM_slides[index].classList.add('c-active');
+}
+
+function prev() {
+    DOM_slides[index].classList.remove('c-active');
+    index = (index - 1 + DOM_slides.length) % DOM_slides.length;
+    DOM_slides[index].classList.add('c-active');
+}
