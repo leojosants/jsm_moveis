62378abdf9ebf1a943e75238e403804dbc69efb7
Leonardo Santos <leonardojosantos@gmail.com>
2024-05-07 21:29:36 -0300

    tempo de video 1:20:30

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 let DOM_closer = document.querySelector('#id-closer');
 let DOM_navbar = document.querySelector('.c-navbar');
 let DOM_cart = document.querySelector('.c-shopping-cart');
+let DOM_loginForm = document.querySelector('.c-login-form');
 
 DOM_closer.onclick = () => {
     DOM_closer.style.display = 'none';
@@ -17,4 +18,9 @@ document.querySelector('#id-menu-btn').onclick = () => {
 document.querySelector('#id-cart-btn').onclick = () => {
     DOM_closer.style.display = 'block';
     DOM_cart.classList.toggle('c-active');
+};
+
+document.querySelector('#id-login-btn').onclick = () => {
+    DOM_closer.style.display = 'block';
+    DOM_loginForm.classList.toggle('c-active');
 };
